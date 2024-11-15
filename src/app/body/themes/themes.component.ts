d7e30b098c00edbe65fc34f22a4a99698e39c9d2
Monika Kiskinova <monikova@abv.bg>
2024-11-15 22:19:43 +0200

    Connecting DB and rendering themes with data from db

--- src/app/body/themes/themes.component.ts
+++ src/app/body/themes/themes.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Theme } from '../../types/theme';
 
 @Component({
   selector: 'app-themes',
   standalone: true,
   imports: [],
+  providers: [ApiService],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.css'
 })
-export class ThemesComponent {
+export class ThemesComponent implements OnInit {
+  themes: Theme[] = [];
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.apiService.getThemes().subscribe((themes) => {
+      this.themes = themes;
+    })
+  }
 }
