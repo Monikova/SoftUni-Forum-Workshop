0e4c15d7e4ccdfeb7eeeeaa5c8069fb7f89d08b9
Monika Kiskinova <monikova@abv.bg>
2024-11-17 00:18:12 +0200

    Adding new resources and rendering 404 error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { BodyComponent } from './body/body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: BodyComponent},
+    {path: "**", component: ErrorPageComponent},
+];
--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-error-page',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './error-page.component.html',
+  styleUrl: './error-page.component.css'
 })
-export class HeaderComponent {
+export class ErrorPageComponent {
 
 }
